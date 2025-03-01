602096ebe4f086f00b15cd0d427abfa317f4176d
Getahun Haftu <getahun19h@gmail.com>
2025-03-01 07:54:21 +0300

    pages and layoutes added

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -3,7 +3,7 @@ import JobListing from "./JobList";
 import jobs from "../jobs.json";
 
 const JobListings = () =>{
-    // console.log(jobs);
+    const recentJob = jobs.slice(0,3);
     return (
         <section className="bg-  px-4 py-10">
             <div className="container-xl lg:container m-auto">
@@ -11,7 +11,7 @@ const JobListings = () =>{
                     Brows Jobs
                 </h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-                {jobs.map((job)=>(
+                {recentJob.map((job)=>(
                  <JobListing key={job.id} job={job}/>
                 ))}
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import React from 'react';
 
 createRoot(document.getElementById('root')).render(
-  < BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>
 )
